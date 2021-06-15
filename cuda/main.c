9b4fd8abae5022aed8019236a1ed1c78f58f94bb
荒川康一 <arakawakouichi@arakk.local>
2021-06-15 10:09:32 +0900

    jfpo

---
+++ cuda/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+int main(int argc, char* argv[]){
+    if(argc != 3) return 1;
+    char* SALT = argv[1];
+    char* PW = argv[2];
+    char result[7];
+
+    return 0;
+}
